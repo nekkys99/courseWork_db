3e11d9046eb4afbaac7f921db1a2cfa32ae2f356
NikitaMalinov <malinov1999@gmail.com>
2022-02-14 20:18:48 +0300

    TryCommit

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 import sqlite3
 import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from FDataBase import FDataBase
 
-
-DATABASE='/tmp/flsite.db'
-DEBUG=True
-SECRET_KEY='fasodjo1iojdasiou092310ajsdp/,m,jjpasdo443'
-
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fasodjo1iojdasiou092310ajsdp/,m,jjpasdo443'
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'fgfgsfgsf21fg214fgfg31sff33as1'
+# app.config['SECRET_KEY'] = 'fgfgsfgsf21fg214fgfg31sff33as1'
 app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 
 def connect_db():
-    conn=sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory=sqlite3.Row
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
     return conn
 
 
 def create_db():
-    db=connect_db()
-    with app.open.resouce('sq_db.sql',mode='r') as f:
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
 
 
 menu = [{"name": "О сайте", "url": "about"},
@@ -77,7 +82,48 @@ def login():
     elif request.method == 'POST' and request.form['username'] == "malinov" and request.form['psw'] == '123':
         session['userLogged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLogged']))
-    return render_template("login.html", title="Авторизация",menu=menu)
+    return render_template("login.html", title="Авторизация", menu=menu)
+
+
+@app.route("/testdb")
+def test_data():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('testdb.html', menu=dbase.getMenu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+    return render_template('post.html',menu=dbase.getMenu(),title=title,post=post)
 
 
 if __name__ == "__main__":
